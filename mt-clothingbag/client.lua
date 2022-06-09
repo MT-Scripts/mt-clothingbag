a2f05a3c3ddaa344390fc5725c37ad58d7d80e01
Marttins <89866234+Marttins011@users.noreply.github.com>
2022-06-09 13:18:50 +0100

    Update client.lua

--- mt-clothingbag/client.lua
+++ mt-clothingbag/client.lua
@@ -56,7 +56,7 @@ RegisterNetEvent('mt-clothing:client:AbrirMenu', function()
     if Config.ClothingMenu == 'qb-clothing' then
         TriggerEvent('qb-clothing:client:openOutfitMenu')
     elseif Config.ClothingMenu == 'fivem-appearance' then
-        TriggerEvent('fivem-appearance:client:changeOutfitMenu')
+        TriggerEvent('fivem-appearance:pickNewOutfit')
     end
 end)
 
